c959fc794ab739c73056084b54ed9816d6361262
johan L <johan27000@gmail.com>
2024-08-30 01:24:45 +0200

    fix linter

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -22,7 +22,7 @@ interface RecaptchaResponse {
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
 });
 
 const ses = new AWS.SES({ apiVersion: '2010-12-01' });
